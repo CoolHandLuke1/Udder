b3017370c2d6267eb5bdd61ac3c09df203910ddf
CoolHandLuke1 <153755125+CoolHandLuke1@users.noreply.github.com>
2024-02-12 11:21:36 -0500

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Lucas Dennis on 2/12/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
